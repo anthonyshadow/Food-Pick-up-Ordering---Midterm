501649e8397022465dc83dff716bb94f9a3ff10b
Karen <karendavtyan21@mail.com>
2019-10-19 17:44:13 +0000

    added login template and login route

--- server.js
+++ server.js
@@ -35,11 +35,12 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-
+const loginRoutes = require("./routes/login")
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/login", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -53,3 +54,8 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.get("/login", (req, res) => {
+  res.render("login")
+})
+
