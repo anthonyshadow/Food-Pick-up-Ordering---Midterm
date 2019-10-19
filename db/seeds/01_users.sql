b31d2fa0635150b75817423218e68c4bb582e20b
nasouh94 <nasouh@live.com>
2019-10-19 19:21:56 +0000

    created the tables for the database, and some seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,25 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+ INSERT INTO users (
+    name, email, password, phone, is_resturant)
+    VALUES (
+    'Devin Sanders', 'tristanjacobs@gmail.com', 'password', 5147919450, false);
+    INSERT INTO users (
+    name, email, password, phone, is_resturant)
+    VALUES (
+    'Iva Harrison', 'allisonjackson@mail.com', 'password', 5147919450, false);
+    INSERT INTO users (
+    name, email, password, phone, is_resturant)
+    VALUES (
+    'Lloyd Jefferson', 'asherpoole@gmx.com', 'password.', 5147919450, false);
+    INSERT INTO users (
+    name, email, password, phone, is_resturant)
+    VALUES (
+    'Dale Coleman', 'michaelgray@mail.com', 'password.', 5147919450, false);
+    INSERT INTO users (
+    name, email, password, phone, is_resturant)
+    VALUES (
+    'Alejandro Osborne', 'ariaatkinson@outlook.com', 'password.', 5147919450, false);
+    INSERT INTO users (
+    name, email, password, phone, is_resturant)
+    VALUES (
+    'Nell Medina', 'juliansantos@aol.com', 'password.', 5147919450, false);
