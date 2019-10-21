aec14a993a9884be224e5bc1b2d86251318aff4a
nasouh94 <nasouh@live.com>
2019-10-21 21:16:37 +0000

    tried pulling the food item into the menu page

--- routes/menu.js
+++ routes/menu.js
@@ -3,8 +3,28 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-  res.render("menu")
+    db.query(`SELECT * FROM foods;`)
+    .then(data => data.rows)
+     res.render("menu")
   });
   return router;
 };
+
+// module.exports = (db) => {
+//   console.log(db)
+//   router.get("/", (req, res) => {
+//     db.query(`SELECT * FROM foods;`)
+//       .then(data => {
+//         const foods = data.rows;
+//         res.json({ foods })
+//       })
+//       .catch(err => {
+//         res
+//           .status(500)
+//           .json({ error: err.message });
+//       });
+//   });
+//   return router;
+// };
+
 
--- server.js
+++ server.js
@@ -40,6 +40,9 @@ const loginRoutes = require("./routes/login");
 const menuRoutes = require("./routes/menu");
 const restaurantRoutes = require("./routes/restaurant");
 const about_usRoutes = require("./routes/about_us");
+const foodRoutes = require("./routes/menu")
+
+
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
@@ -48,6 +51,7 @@ app.use("/login", loginRoutes(db));
 app.use("/menu", menuRoutes(db));
 app.use("/restaurant", restaurantRoutes(db));
 app.use("/about_us", about_usRoutes(db));
+app.use("/api/foods", foodRoutes(db))
 // Note: mount other resources here, using the same pattern above
 
 
