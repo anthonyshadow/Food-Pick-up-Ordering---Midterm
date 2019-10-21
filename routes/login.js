fae43807ed3c3a31ccbf142875a79ccf7c18813b
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-10-21 21:16:23 +0000

    edited routes and nav.ejs to allow login feature and change nav bar to show user signed in when authenticated

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,28 @@
 const express = require('express');
+const app = express();
 const router  = express.Router();
 const cookieSession = require('cookie-session');
 
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
+  let templateVars = {}
+  templateVars.user = req.session.user_id ? req.session.user_id : undefined;
   db.query(`SELECT name FROM users;`)
   .then(data => data.rows)
-    res.render("login")
+    res.render("login", templateVars)
   });
   router.post("/", (req, res) => {
     db.query(`SELECT * FROM users WHERE email = $1`, [req.body.email])
     .then(data => {
       const user = data.rows[0]
-      let templateVars;
       if(user && user.password === req.body.password) {
-        templateVars = { email: req.body.email, password: req.body.password };
-        res.redirect("/", templateVars);
+        req.session.user_id = user;
+        res.redirect("/");
       } else {
         res.send("Wrong password or email")
       }
     })
-    res.render("/");
     });
   return router;
 };
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const cookieSession = require('cookie-session');
+const app = express();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
