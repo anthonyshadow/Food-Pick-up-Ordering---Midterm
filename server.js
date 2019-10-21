b592425f2e5c0133d0c8a55ba848076c8ccf0de8
Karen <karendavtyan21@mail.com>
2019-10-21 15:21:39 +0000

    added login and register routes

--- server.js
+++ server.js
@@ -35,12 +35,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const registerRoutes = require("./routes/register");
 const loginRoutes = require("./routes/login")
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-app.use("/api/login", loginRoutes(db));
+app.use("/register", registerRoutes(db));
+app.use("/login", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -48,7 +50,6 @@ app.use("/api/login", loginRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-
   res.render("index");
 });
 
