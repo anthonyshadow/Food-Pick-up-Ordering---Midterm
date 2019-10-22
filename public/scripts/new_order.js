e7638448a7888c9d80cb53f3cb4452d71ba593bd
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-10-22 16:16:37 +0000

    edited information being displayed within ejs files. Began to add function to render new order

---
+++ public/scripts/new_order.js
@@ -0,0 +1,10 @@
+const createNewOrder = function (order) {
+  const $order = `
+  <article class="new-order">
+    <span>ORDER ID</span>
+    <span>ORDER DATE</span>
+    <span>STATUS</span>
+  </article>
+  `;
+  return $order
+}
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -5,7 +5,9 @@ const app = express();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-  res.render("restaurant")
+    let templateVars = {}
+    templateVars.user = req.session.user_id ? req.session.user_id : undefined;
+  res.render("restaurant", templateVars)
   });
   return router;
 };
