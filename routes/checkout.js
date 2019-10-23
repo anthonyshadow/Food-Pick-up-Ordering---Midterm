07707239d1c97f31815e9b8b4a0df56d4b6c3696
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-10-23 22:12:30 +0000

    push new orders to database from checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -25,6 +25,11 @@ module.exports = (db) => {
     res.render("checkout", templateVars)
   });
   router.post("/", (req, res) => {
+    let { user_id, ordered_at, accepted, completed, total_price, customer_comment} = req.body
+
+    db.query(`INSERT into ORDERS (user_id, total_price)
+    VALUES ($1, $2)`, [user_id, total_price])
+
     db.query(`
     UPDATE orders
     SET accepted = true
