e2fc6394c74638cc4d239f357cb46c7d6048e044
nasouh94 <nasouh@live.com>
2019-10-23 18:30:37 +0000

    add to cart adds item to the cart but deletes after referesh

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,13 +26,15 @@
 $( document ).ready(function() {
   let $button = $('.add-to-cart');
   $button.on("click", function(){
+    let foodImage = $(this).closest(".card").find('.food-image').text()
     let foodName = $(this).closest(".card").find('.food-name').text()
     let foodDescription = $(this).closest(".card").find('.food-description').text()
     let foodPrice = $(this).closest(".card").find('.food-price').text()
       let cartItem = {
         foodName: foodName,
         foodDescription: foodDescription,
-        foodPrice: foodPrice
+        foodPrice: foodPrice,
+        foodImage: foodImage
       }
       let cartItems
       if(localStorage["cart-item"] === undefined) {
@@ -42,20 +44,41 @@ $( document ).ready(function() {
       }
       cartItems.push(cartItem)
       localStorage.setItem("cart-item", JSON.stringify(cartItems))
+      addToCart(cartItem)
   })
 });
 
 
+function createCartElement(cartItem) {
+  const $cartItem = `
+  <article class="cart-item">
+      <tbody>
+          <tr>
+              <td data-th="Product">
+                  <div class="row">
+                      <div class="col-sm-2 hidden-xs"><img src="${cartItem.foodImage}" alt="..." class="img-responsive" /></div>
+                      <div class="col-sm-10">
+                          <h4 class="nomargin">${cartItem.foodName}</h4>
+                          <p>${cartItem.foodDescription}</p>
+                      </div>
+                  </div>
+              </td>
+              <td data-th="Price">$${cartItem.foodPrice}</td>
+              <td data-th="Quantity">
+                  <input type="number" class="form-control text-center" value="1">
+              </td>
+              <td data-th="Subtotal" class="text-center">1.99</td>
+              <td class="actions" data-th="">
+                  <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+                  <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>
+              </td>
+          </tr>
+      </tbody>
+    </article>
+  `;
+  return $cartItem
+}
 
-
-//i was trying to make it work with ajax 
-
-// $(() => {
-//   $.ajax({
-//     method: "GET",
-//     url: "app.js",
-//     data: "cart-item"
-//   }).done((data) => {
-//       $("<div>").text(data).appendTo($(".menu-info"));
-//   });
-// });
+function addToCart(cartItems) {
+  $("#cart").append(createCartElement(cartItems))
+}
